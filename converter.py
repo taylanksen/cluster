b4fe84072f0ff7b4019db907e5656a4dddf355d8
nhatminh886688 <mtran14@u.rochester.edu>
2017-08-03 16:06:50 +0700

    add gmm code to get cluster sequence

--- converter.py
+++ converter.py
@@ -9,6 +9,7 @@ import csv
 import numpy as np
 import pandas as pd
 from sklearn import cluster
+from sklearn import mixture
 
 import matplotlib.pyplot as plt
 
@@ -17,7 +18,7 @@ import os
 import sys
 import argparse
 import logging
-
+import re
 
 logging.basicConfig(level=logging.DEBUG)
 # you can set the logging level from a command-line option such as:
@@ -54,13 +55,80 @@ class ClusterConverter:
             f.write(str(k_means.predict(s.data))[1:-1]) #[1:-1] to remove the '[]'
 
         print('Cluster Index Sequnce written to: {}'.format(outfile))
+        
+    """ """
+def load_gmm_cluster(infile):
+    """ loads a gmm cluster file into a df """
+    print("Reading cluster definition from: {}".format(infile))
+    cluster_list = []
+    with open(infile) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        
+        for row in readCSV:
+            cluster_list.append(row)
     
+    au06 =[]
+    au12 =[]
+    sigmas = []
+    means = []
+    for i in range(1, len(cluster_list)):
+        au06.append(np.float(cluster_list[i][0]))
+        au12.append(np.float(cluster_list[i][1]))
+        temp = cluster_list[i][2].split('\n')
+        first = re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", temp[0])
+        y1 = (np.array(first)).astype(np.float)
+        y1 =y1.reshape(1,2)
+        second = re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", temp[1])
+        y2  = (np.array(second)).astype(np.float)
+        y2 =y2.reshape(1,2)
+        cov = np.concatenate((y1,y2), axis=0)
+        sigmas.append(cov)
+    
+    
+    for i in range(0,len(au06)):
+        means.append([au06[i],au12[i]])
+    means = np.array(means)
+    sigmas = np.array(sigmas)
+    print("Cluster centers:\n", means)
+    print("Cluster covariance:\n", sigmas)
+    
+    return means,sigmas
+
+def write_gmm_cluster_sequence(means, sigmas, outfile, targetFile):
+    """ uses GMM prediction to print sequence of closest clusters """
+    k = means.shape[0]
+    features=[' AU06_r',' AU12_r']
+    df = pd.read_csv('example/test.csv')
+    X = df.loc[:,features].dropna().values
+    gmm = mixture.GaussianMixture(n_components=k,
+                                          covariance_type='full',
+                                          tol=1e-8,
+                                          max_iter=1000,
+                                          n_init=3,
+                                          reg_covar=2e-3)
+    gmm.fit(X)
+    gmm.means_ = means
+    gmm.covariances_ =sigmas
+    
+    df1 = pd.read_csv(targetFile)
+    data = df.loc[:,features].dropna().values
+    
+    output = gmm.predict(data)
+    with open(outfile, 'w') as f:
+        #f.write(str(gmm.predict(data))[1:-1]) 
+        for i in range(0, output.shape[0]):
+            f.write(str(output[i])+' ')
+        
+    print('Cluster Index Sequnce written to: {}'.format(outfile))
+            
 #------------------------------------------------------------------------
 def do_all(args):
     cluster_converter = ClusterConverter()    
-    cluster_converter.load_cluster(args.c)
-    cluster_converter.load_data_sequence(args.d)
-    cluster_converter.write_cluster_sequence(args.o)
+    #cluster_converter.load_cluster(args.c)
+    #cluster_converter.load_data_sequence(args.d)
+    #cluster_converter.write_cluster_sequence(args.o)
+    means, sigmas = load_gmm_cluster(args.c)
+    write_gmm_cluster_sequence(means, sigmas, args.o, args.t)
 
 #------------------------------------------------------------------------
 if __name__ == '__main__':
@@ -69,12 +137,16 @@ if __name__ == '__main__':
     help_intro = 'Program for converting a seq datafile into a seq of cluster indices.' 
     parser = argparse.ArgumentParser(description=help_intro)
 
+    #parser.add_argument('-c',help='cluster_file, ex:example/face_clusters.csv',\
+    #                    type=str, default='example/face_clusters_AU06_r_AU12_r_4.csv')    
     parser.add_argument('-c',help='cluster_file, ex:example/face_clusters.csv',\
-                        type=str, default='example/face_clusters_AU06_r_AU12_r_4.csv')
+                        type=str, default='test_gmm_5_inp.csv')
     parser.add_argument('-d', help='data_file, ex:example/test.csv', \
                         type=str, default='example/test.csv')
     parser.add_argument('-o', help='output_filename, ex:output/test.seq', \
-                        type=str, default='output/test.seq')
+                        type=str, default='output/test_gmm.seq')
+    parser.add_argument('-t', help='output_filename, ex:output/test.seq', \
+                        type=str, default='example/test.csv')    
     args = parser.parse_args()
     
     print('args: ', args)
